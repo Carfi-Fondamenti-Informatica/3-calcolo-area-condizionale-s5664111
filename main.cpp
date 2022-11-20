bb2a2e5989d8e5653e146e45b4b2c89f187e4c8b
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-11-20 20:32:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,20 @@ using namespace std;
 int main() {
 float a=0, b=0;
 int opzione=0;
-float area=0;
 cin>>a>>b>>opzione;
 switch(opzione){
       case0:
-      area=(a*b)/2;
+      (a*b)/2;
+       cout<<(a*b)/2<<endl;     
       break;
       case1:
-      area=a*a;
+    cout<<a*a<<endl;
          break;
       case2:
-      area=a*b;
+  cout<<a*b<<endl;
       break;
    default:
       cout<<"opzione non valida"<<endl;
 }
-cout<<area<<endl;
-
    return 0;
 }
