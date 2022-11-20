db62d98938e63b6ffce8f70dead7477741f05fbd
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-11-20 20:38:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@ using namespace std;
 int main() {
 float a=0, b=0;
 int opzione=0;
+
 cin>>a>>b>>opzione;
 switch(opzione){
       case0:
-            float area;
+            float area=0;
             area=(a*b)/2
        cout<<area<<endl;     
       break;
       case1:
-            float area;
+            float area=0;
             area=a*a
     cout<<area<<endl;
          break;
       case2:
-            float area;
+            float area=0;
             area=a*b
   cout<<area<<endl;
       break;
